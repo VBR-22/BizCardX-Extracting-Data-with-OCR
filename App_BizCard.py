870b7628b744fb29e09afe178dd336f635bb3d7b
Ramani Shankar VB <128304280+VBR-22@users.noreply.github.com>
2023-04-29 21:03:07 +0530

    Update App_BizCard.py

--- App_BizCard.py
+++ App_BizCard.py
@@ -7,7 +7,7 @@ import re
 import pandas as pd
 
 #title
-st.title(":blue[BizCardX-Extracting-Business-Card-Data-with-OCR]") 
+st.title(":red[BusinessCardX-Extracting-Data]") 
 st.write(" ")
 col1, col2,col3= st.columns([3,0.5,4.5])
 with col1:
